fca3dda424e9539586a0382be536044c44ae11d0
Matthias Deja <MadocDoyu@gmail.com>
2017-06-08 13:52:49 +0200

    adjusted the test class to the recent changes

--- src/main/scala/com/github/madoc/runsbt/TestMe.scala
+++ src/main/scala/com/github/madoc/runsbt/TestMe.scala
@@ -1,13 +1,15 @@
 package com.github.madoc.runsbt
 
+import java.io.File
+
 import com.github.madoc.runsbt.config.ExecutableConfig.CommandLineExecutableConfig
 import com.github.madoc.runsbt.config.SBTConfig
-import com.github.madoc.runsbt.running.SBTCommand
+import com.github.madoc.runsbt.running.SBTCommand.CompileCommand
 
 object TestMe extends App {
   def runOnce() {
     val runSBT = RunSBT(SBTConfig(CommandLineExecutableConfig("/Users/madoc/bin/sbt")))
-    val proc = runSBT(SBTCommand.FreeForm("-h"))
+    val proc = runSBT(new File("/Users/madoc/tmp/testprj"), CompileCommand)
     proc.outputLines foreach println
   }
 
