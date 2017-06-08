94ac37005e65d5856035415f0a833a756bc26f76
Matthias Deja <MadocDoyu@gmail.com>
2017-06-08 13:51:54 +0200

    added CompileCommand

--- src/main/scala/com/github/madoc/runsbt/running/SBTCommand.scala
+++ src/main/scala/com/github/madoc/runsbt/running/SBTCommand.scala
@@ -4,7 +4,9 @@ sealed trait SBTCommand {
   def toStringSeq:Seq[String]
 }
 object SBTCommand {
-  sealed case class FreeForm(text:String) extends SBTCommand {def toStringSeq = Seq(text)}
+  sealed case class FreeFormCommand(text:String) extends SBTCommand {def toStringSeq = Seq(text)}
+
+  object CompileCommand extends SBTCommand {def toStringSeq = Seq("compile")}
   object PublishLocalCommand extends SBTCommand {def toStringSeq = Seq("publish-local")}
   object TestCommand extends SBTCommand {def toStringSeq = Seq("test")}
 }
